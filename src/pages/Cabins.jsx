cee56b01f2556dbf2838dc8e1f30e64d0667c05a
Alireza-my <alireza.mahmoodi.amai@gmail.com>
2023-10-30 11:30:13 +0330

    Adding filter table by discount

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,13 +2,14 @@ import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabins from "../features/cabins/AddCabins";
+import CabinTableOperation from "../features/cabins/CabinTableOperation";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperation />
       </Row>
 
       <Row>
