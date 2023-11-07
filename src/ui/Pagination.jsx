cee1c99445ad0afac870a804911f7b2cfcb4d6c3
Alireza-my <alireza.mahmoodi.amai@gmail.com>
2023-11-07 08:38:44 +0330

    Add pagination logic to pagination component

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -58,8 +59,6 @@ const PaginationButton = styled.button`
   }
 `;
 
-const PAGE_SIZE = 10;
-
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -83,7 +82,7 @@ function Pagination({ count }) {
     setSearchParams(searchParams);
   }
 
-  if (pageCount <= 10) return null;
+  if (pageCount <= 1) return null;
 
   return (
     <StyledPagination>
